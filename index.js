f016d90ff7a887f0ccddad6032bf062631a2df10
tomasdargis <167984812+tomasdargis@users.noreply.github.com>
2024-08-29 23:17:55 +0300

    api

--- index.js
+++ index.js
@@ -3,14 +3,25 @@ import { serviceRouter } from './router/serviceRouter.js';
 import { mokiniaiRouter } from './router/mokiniaiRouter.js';
 import { bookRouter } from './router/bookRouter.js';
 import { carsRouter } from './router/carsRouter.js';
+import { apiRouter } from './router/apiRauter.js';
+
 const app = express();
 const port = 3000;
+
+// for parsing application/json
+app.use(
+    express.json({
+        type: 'application/json',
+    })
+);
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     return res.send('Labas vakaras, Lietuva!');
 });
 
-app.use('/api', apiRauter);
+app.use('/api', apiRouter);
 app.use('/service', serviceRouter);
 app.use('/mokiniai', mokiniaiRouter);
 app.use('/book', bookRouter);
--- router/apiRauter.js
+++ router/apiRauter.js
@@ -1,8 +1,18 @@
 import express from 'express';
 
-export const apiRauter = express.Router();
+export const apiRouter = express.Router();
 
+const marks = [];
 
-apiRauter.get('/', (req.res) => {
-    return 
+apiRouter.get('/my-marks', (req, res) => {
+    return res.json(marks);
+});
+apiRouter.post('/my-marks', (req, res, next) => {
+    console.log(req.body.marks);
+    marks.push(req.body.marks);
+
+    return res.json({
+        state: 'Pavyko',
+        message: 'Pazymys pridetas',
+    });
 });
